4561c6f688a3a11edf5fa7f738bd6f19b4647dcd
Lisa Ugray <lisa.ugray@shopify.com>
2019-12-12 15:02:51 -0500

    Skip testing long day12

--- 2019/test.rb
+++ 2019/test.rb
@@ -8,6 +8,7 @@ class TestDays < Minitest::Test
       basename = File.basename(f, '.rb')
       outfile = "#{basename}.output"
       skip "Missing output file #{outfile}" unless File.exist?(outfile)
+      skip "Too long" if basename == 'day12'
       require_relative basename
       assert_equal File.read(outfile), Object.const_get(basename.capitalize).output
     end
