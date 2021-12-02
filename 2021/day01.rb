22bc7cecd88362810501327f44de280e05bba9ff
Lisa Ugray <lisa.ugray@shopify.com>
2021-12-02 08:51:52 -0500

    Day 1

---
+++ 2021/day01.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require_relative 'day'
+
+class Day01 < Day
+  def initialize
+    @depths = input.each_line.map(&:to_i)
+  end
+
+  def part_1
+    @depths.each_cons(2).count { |a, b| b > a }
+  end
+
+  def part_2
+    @depths.each_cons(4).count { |a, _, _, b| b > a }
+  end
+end
+
+Day01.run if __FILE__ == $0
