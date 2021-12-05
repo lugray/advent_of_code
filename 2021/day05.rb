6e1eca4868912d5e08b8551821ca2088f5a6ef2c
Lisa Ugray <lisa.ugray@shopify.com>
2021-12-05 07:07:10 -0500

    Day 5

---
+++ 2021/day05.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+require_relative 'day'
+
+class Day05 < Day
+  class Vent
+    attr_reader :x1, :y1, :x2, :y2
+
+    def initialize(x1, y1, x2, y2)
+      @x1 = x1
+      @y1 = y1
+      @x2 = x2
+      @y2 = y2
+    end
+
+    def horizontal?
+      y1 == y2
+    end
+
+    def vertical?
+      x1 == x2
+    end
+
+    def each_point
+      delta_x = x2 <=> x1
+      delta_y = y2 <=> y1
+      size = [(x2 - x1).abs, (y2 - y1).abs].max
+      (0..size).each do |d|
+        yield([x1 + d * delta_x, y1 + d * delta_y])
+      end
+    end
+  end
+
+  def initialize
+    @vents = input_lines.map do |l|
+      vals = l.split(' -> ').flat_map do |pair|
+        pair.split(',').map(&:to_i)
+      end
+      Vent.new(*vals)
+    end
+  end
+
+  def count_overlaps(vents)
+    vent_counts = Hash.new { 0 }
+    vents.each do |v|
+      v.each_point { |p| vent_counts[p] += 1 }
+    end
+
+    vent_counts.count { |_, v| v > 1 }
+  end
+
+  def part_1
+    count_overlaps(@vents.select { |v| v.horizontal? || v.vertical? })
+  end
+
+  def part_2
+    count_overlaps(@vents)
+  end
+end
+
+Day05.run if __FILE__ == $0
