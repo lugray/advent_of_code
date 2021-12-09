1cdd11c42ff653a1269a277a97c445e1316ea288
Lisa Ugray <lisa.ugray@shopify.com>
2021-12-09 09:15:09 -0500

    2015 Day 24 - Faster: limit size to 1/n of packages
    Takes time from 16s to 5s

--- 2015/day24.rb
+++ 2015/day24.rb
@@ -8,15 +8,16 @@ class Day24 < Day
     @weights = input.each_line.map(&:to_i)
   end
 
-  def groups(set, target)
-    return enum_for(:groups, set, target) unless block_given?
+  def groups(set, target, max_size)
+    return enum_for(:groups, set, target, max_size) unless block_given?
     if target == 0
       yield Set.new
       return
     end
+    return if max_size == 0
     set.each_with_index do |elem, i|
       next if elem > target
-      groups(set[i+1..], target - elem).each do |inner_set|
+      groups(set[i+1..], target - elem, max_size - 1).each do |inner_set|
         yield Set.new([elem]) + inner_set
       end
     end
@@ -24,7 +25,7 @@ class Day24 < Day
 
   def best_qe(set, n)
     qe(
-      groups(set, set.sum / n).min do |g1, g2|
+      groups(set, set.sum / n, set.size / n).min do |g1, g2|
         (g1.size <=> g2.size).nonzero? || (qe(g1) <=> qe(g2))
       end
     )
