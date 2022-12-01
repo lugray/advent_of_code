ba5f57e1afc6340f7f84240f0fb3b0e0abf06876
Lisa Ugray <lisa.ugray@shopify.com>
2022-12-01 08:57:23 -0500

    Set up 2022

---
+++ 2022/day.rb
@@ -0,0 +1,30 @@
+class Day
+  def input
+    File.read("#{self.class.to_s.downcase}.input")
+  end
+
+  def input_lines
+    input.each_line.map(&:chomp)
+  end
+
+  def input_numbers
+    if input.include?(',')
+      input.split(',').map(&:to_i)
+    else
+      input_lines.map(&:to_i)
+    end
+  end
+
+  class << self
+    def run
+      day = new
+      puts day.part_1
+      puts day.part_2
+    end
+
+    def output
+      day = new
+      [day.part_1, day.part_2].join("\n")
+    end
+  end
+end
---
+++ 2022/test.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require "minitest/autorun"
+require "minitest/reporters"
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class TestDays < Minitest::Test
+  Dir.glob(File.join(File.dirname(__FILE__), 'day??.rb')).sort.each do |f|
+    basename = File.basename(f, '.rb')
+    define_method("test_#{basename}") do
+      outfile = "#{basename}.output"
+      skip "Missing output file #{outfile}" unless File.exist?(outfile)
+      require_relative basename
+      assert_equal File.read(outfile), Object.const_get(basename.capitalize).output
+    end
+  end
+end
