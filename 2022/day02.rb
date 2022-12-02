85793ad931ffb077c1c8f4d1a27d2b65d1524aea
Lisa Ugray <lisa.ugray@shopify.com>
2022-12-02 11:33:07 -0500

    Day 2 refactor

--- 2022/day02.rb
+++ 2022/day02.rb
@@ -3,60 +3,44 @@
 require_relative 'day'
 
 class Day02 < Day
+  CHOICE_POINTS = { rock: 1, paper: 2, scissors: 3 }
+  RESULT_POINTS = { win: 6, draw: 3, loss: 0 }
+  BEATS = { rock: :paper, paper: :scissors, scissors: :rock }
+  LOOSES_TO = BEATS.invert
+  CHOICE = { 'A' => :rock, 'B' => :paper, 'C' => :scissors, 'X' => :rock, 'Y' => :paper, 'Z' => :scissors }
+  INSTRUCTION = { 'X' => :loss, 'Y' => :draw, 'Z' => :win }
+  TO_GET = {
+    win: ->(opp_choice) { BEATS[opp_choice] },
+    draw: ->(opp_choice) { opp_choice },
+    loss: ->(opp_choice) { LOOSES_TO[opp_choice] },
+  }
+
   def initialize
     @rounds = input_lines
   end
 
-  def choice_points(choice)
-    case choice
-      when 'R'; 1
-      when 'P'; 2
-      when 'S'; 3
-    end
-  end
-
-  def result_points(result)
-    case result
-      when 'W'; 6
-      when 'D'; 3
-      when 'L'; 0
-    end
-  end
-
   def score(choice, result)
-    choice_points(choice) + result_points(result)
+    CHOICE_POINTS[choice] + RESULT_POINTS[result]
   end
 
   def result(opp_choice, my_choice)
-    case [opp_choice, my_choice].join.tr('RPS', '123').chars.map(&:to_i).reduce(:-).modulo(3)
-      when 0; 'D'
-      when 1; 'L'
-      when 2; 'W'
-    end
+    TO_GET.transform_values { |f| f.call(opp_choice) }.invert[my_choice]
   end
 
   def pick(opp_choice, instruction)
-    options = %w(R P S)
-    opp_index = options.index(opp_choice)
-    case instruction
-      when 'L'; options[(opp_index - 1) % 3]
-      when 'D'; opp_choice
-      when 'W'; options[(opp_index + 1) % 3]
-    end
+    TO_GET[instruction].call(opp_choice)
   end
 
   def part_1
     @rounds.sum do |line|
-      opp_choice, my_choice = line.tr('ABCXYZ', 'RPSRPS').split(' ')
+      opp_choice, my_choice = line.split(' ').map { |c| CHOICE[c] }
       score(my_choice, result(opp_choice, my_choice))
     end
   end
 
   def part_2
     @rounds.sum do |line|
-      opp_choice, instruction = line.split(' ')
-      instruction.tr!('XYZ', 'LDW')
-      opp_choice = opp_choice.tr('ABC', 'RPS')
+      opp_choice, instruction = line.split(' ').then { |c, i| [CHOICE[c], INSTRUCTION[i]] }
       my_choice = pick(opp_choice, instruction)
       score(my_choice, instruction)
     end
