c6410cba64eb96751ebb620250770f017d23963e
Lisa Ugray <lisa.ugray@gmail.com>
2023-12-04 08:50:28 -0500

    Day 4 - refactor

--- 2023/day04.rb
+++ 2023/day04.rb
@@ -3,32 +3,23 @@
 require_relative 'day'
 
 class Day04 < Day
-  def part_1
-    input_lines.sum do |line|
-      _, nums = line.split(': ')
-      win, have = nums.split(' | ')
-      win = win.split(' ').map(&:to_i)
-      have = have.split(' ').map(&:to_i)
-      c = have.count { |n| win.include?(n) }
-      c == 0 ? 0 : 2**(c - 1)
+  def initialize
+    @cards = input_lines.each_with_object({}) do |line, cards|
+      id, nums = line.split(': ')
+      win, have = nums.split(' | ').map { |set| set.split(' ').map(&:to_i) }
+      cards[id.split(' ').last.to_i] = have.count { |n| win.include?(n) }
     end
   end
 
+  def part_1
+    @cards.sum { |_, wins| wins == 0 ? 0 : 2**(wins-1) }
+  end
+
   def part_2
-    card_counts = Hash.new(0)
-    input_lines.each do |line|
-      card, nums = line.split(': ')
-      card_num = card.split(' ').last.to_i
-      card_counts[card_num] += 1
-      win, have = nums.split(' | ')
-      win = win.split(' ').map(&:to_i)
-      have = have.split(' ').map(&:to_i)
-      c = have.count { |n| win.include?(n) }
-      ((card_num+1)..card_num+c).each do |n|
-        card_counts[n] += card_counts[card_num]
-      end
-    end
-    card_counts.values.sum
+    @cards.each_with_object(Hash.new(0)) do |(id, wins), card_counts|
+      card_counts[id] += 1
+      wins.times { |n| card_counts[id+n+1] += card_counts[id] }
+    end.values.sum
   end
 end
 
