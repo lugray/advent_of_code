526ce1608ff1923ad6e20c68bd0085addff168c4
Lisa Ugray <lisa.ugray@gmail.com>
2023-12-09 07:44:58 -0500

    Day 9 - refactor

--- 2023/day09.rb
+++ 2023/day09.rb
@@ -12,17 +12,12 @@ class Day09 < Day
     sequence.last + next_in(sequence.each_cons(2).map { |a, b| b - a })
   end
 
-  def prev_in(sequence)
-    return 0 if sequence.all?(0)
-    sequence.first - prev_in(sequence.each_cons(2).map { |a, b| b - a })
-  end
-
   def part_1
     @sequences.sum { |s| next_in(s) }
   end
 
   def part_2
-    @sequences.sum { |s| prev_in(s) }
+    @sequences.sum { |s| next_in(s.reverse) }
   end
 end
 
