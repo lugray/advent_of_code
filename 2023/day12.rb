6fa91d63e4bb2be5ee706a02daa5dfa1b0f45745
Lisa Ugray <lisa.ugray@gmail.com>
2023-12-14 08:13:09 -0500

    Day 12

--- 2023/day12.rb
+++ 2023/day12.rb
@@ -3,38 +3,78 @@
 require_relative 'day'
 
 class Day12 < Day
-  def candidates(str, group_sum)
-    return enum_for(:candidates, str, group_sum) unless block_given?
-    i = str.index('?')
-    return yield str unless i
-
-    tallies = str.each_char.tally
-    if tallies.fetch('#', 0) + tallies.fetch('?', 0) == group_sum
-      return yield str.gsub('?', '#')
-    elsif tallies['#'] == group_sum
-      return yield str.gsub('?', '.')
-    end
-
-    candidates(str.sub('?', '.'), group_sum) { |s| yield s }
-    candidates(str.sub('?', '#'), group_sum) { |s| yield s }
-  end
-
-  def groups_from(str)
-    str.split(/\.+/).reject(&:empty?).map(&:length)
-  end
-
   def initialize
     @rows = input_grid
+    @count_possibilities = {}
+  end
+
+  def count_possibilities(conditions, groups)
+    @count_possibilities[[conditions, groups]] ||= count_possibilities!(conditions, groups)
+  end
+
+  def fast_zero?(conditions, groups)
+    @count_possibilities[[conditions, groups]] == 0
+  end
+
+  def medium_zero?(conditions, groups)
+    case @count_possibilities[[conditions, groups]]
+    when 0
+      true
+    when nil
+      if medium_zero!(conditions, groups)
+        @count_possibilities[[conditions, groups]] = 0
+        true
+      else
+        @count_possibilities[[conditions, groups]] = false # Memoize non-medium zero so we can answer more quickly next time, but still allow overwriting the full calculation
+        false
+      end
+    else
+      false
+    end
+  end
+
+  def medium_zero!(conditions, groups)
+    gs = groups.sum
+    (cc = conditions.count('#')) > gs || cc + conditions.count('?') < gs
+  end
+
+  def count_possibilities!(conditions, groups)
+    if groups.empty?
+      return conditions.count('#') == 0 ? 1 : 0
+    end
+
+    lg = groups.max
+    lgi = groups.index(lg)
+
+    (0..(conditions.size - lg)).sum do |i|
+      next 0 unless i == 0 || conditions[i-1] != '#'
+      next 0 unless i + lg == conditions.size || conditions[i+lg] != '#'
+      next 0 unless conditions[i...(i+lg)].each_char.none? { |c| c == '.' }
+
+      right_args = [conditions[i+lg+1..] || '', groups[(lgi+1)..]]
+      left_args = [conditions[...[i-1,0].max], groups[...lgi]]
+      next 0 if fast_zero?(*right_args) || fast_zero?(*left_args) || medium_zero?(*right_args) || medium_zero?(*left_args)
+      right = count_possibilities(*right_args)
+      next 0 if right == 0
+      left = count_possibilities(*left_args)
+      left * right
+    end
   end
 
   def part_1
     @rows.sum do |conditions, groups|
       groups = groups.split(',').map(&:to_i)
-      candidates(conditions, groups.sum).count { |c| groups_from(c) == groups }
+      count_possibilities(conditions, groups)
     end
   end
 
   def part_2
+    @rows.sum do |conditions, groups|
+      groups = groups.split(',').map(&:to_i)
+      conditions = ([conditions]*5).join('?')
+      groups = groups * 5
+      count_possibilities(conditions, groups)
+    end
   end
 end
 
