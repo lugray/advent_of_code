5bc4436d92f9e6c154f365feb6f2cd6ddd2ca690
Lisa Ugray <lisa.ugray@gmail.com>
2024-12-04 10:39:08 -0500

    Day 04 - refactor

--- 2024/day04.rb
+++ 2024/day04.rb
@@ -7,11 +7,11 @@ class Day04 < Day
     @word_search = input_lines.map { |line| line.each_char.to_a }
   end
 
-  def count_horizontal(word_search, word = 'XMAS')
+  def count_horizontal(word_search, word)
     word_search.sum { |line| line.join('').scan(/(?=#{word})/).count }
   end
 
-  def count_diagonal(word_search, word = 'XMAS')
+  def count_diagonal(word_search, word)
     (0..word_search.size - word.size).sum do |i|
       (0..word_search[0].size - word.size).count do |j|
         (0...word.size).all? { |k| word_search[i + k][j + k] == word[k] }
@@ -19,27 +19,24 @@ class Day04 < Day
     end
   end
 
-  def count_all(word_search, word = 'XMAS')
-    count_diagonal(@word_search) +
-      count_diagonal(@word_search.reverse) +
-      count_diagonal(@word_search.map(&:reverse)) +
-      count_diagonal(@word_search.reverse.map(&:reverse)) +
-      count_horizontal(@word_search) +
-      count_horizontal(@word_search.map(&:reverse)) +
-      count_horizontal(@word_search.transpose) +
-      count_horizontal(@word_search.transpose.map(&:reverse))
+  def count_all(word_search, word)
+    4.times.sum do
+      word_search = word_search.transpose.reverse # rotate 90 degrees
+      count_horizontal(word_search, word) + count_diagonal(word_search, word)
+    end
   end
 
   def part_1
-    count_all(@word_search)
+    count_all(@word_search, 'XMAS')
   end
 
   def part_2
-    (0..@word_search.size - 3).sum do |i|
-      (0..@word_search[0].size - 3).count do |j|
-        a = (0...3).map { |k| @word_search[i + k][j + k] }.join
-        b = (0...3).map { |k| @word_search[i + k][j + 2 - k] }.join
-        (["MAS", "SAM"] + [a, b]).uniq.size == 2
+    word = 'MAS'
+    (0..@word_search.size - word.size).sum do |i|
+      (0..@word_search[0].size - word.size).count do |j|
+        a = (0...3).map { |k| @word_search[i + k][j + k] }.join # \
+        b = (0...3).map { |k| @word_search[i + k][j + 2 - k] }.join # /
+        ([word, word.reverse] + [a, b]).uniq.size == 2
       end
     end
   end
