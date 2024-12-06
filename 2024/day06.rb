8e36767c2eac31f02b9e0ab5e1827f3ff67ea546
Lisa Ugray <lisa.ugray@gmail.com>
2024-12-06 12:54:35 -0500

    Day 06 - Refactor 2

--- 2024/day06.rb
+++ 2024/day06.rb
@@ -12,55 +12,42 @@ class Day06 < Day
   def initialize
     @obstacles = {}
     @visited = {}
-    @guard = nil
     @facing = UP
     input_lines.each_with_index do |line, i|
       line.each_char.each_with_index do |c, j|
         @visited[[i, j]] = false
         case c
-        when '#'
-          @obstacles[[i, j]] = true
-        when '^'
-          @guard = [i, j]
+        when '#' then @obstacles[[i, j]] = true
+        when '^' then @guard = [i, j]
         end
       end
     end
   end
 
-  def next_straight(guard, facing)
-    [guard[0] + DIRS[facing][0], guard[1] + DIRS[facing][1]]
-  end
+  def next_straight(guard, facing) = [guard[0] + DIRS[facing][0], guard[1] + DIRS[facing][1]]
+  def new_obstacle(obstacles, new_pos) = obstacles.dup.tap { |h| h[new_pos] = true }
 
-  def looping?(obstacles, visited, guard, facing)
-    until visited[guard] == facing || visited[guard].nil?
+  def loop_count(obstacles, visited, guard, facing, add_obstacles = false)
+    lc = 0
+    loop do
       visited[guard] = facing
       facing = (facing + 1) % 4 while obstacles[next_straight(guard, facing)]
-      guard = next_straight(guard, facing)
-    end
-    visited[guard] == facing
-  end
-
-  def loop_count(obstacles, visited, guard, facing)
-    loop_count = 0
-    until visited[guard] == facing || visited[guard].nil?
-      facing = (facing + 1) % 4 while obstacles[next_straight(guard, facing)]
-      if visited[next_straight(guard, facing)] == false
-        new_obstacles = obstacles.dup.tap { |h| h[next_straight(guard, facing)] = true }
-        loop_count += 1 if looping?(new_obstacles, visited.dup, guard.dup, facing.dup)
+      if add_obstacles && visited[next_straight(guard, facing)] == false
+        lc += loop_count(new_obstacle(obstacles, next_straight(guard, facing)), visited.dup, guard, facing, false)
       end
-      visited[guard] = facing
       guard = next_straight(guard, facing)
+      break if visited[guard] == facing || visited[guard].nil?
     end
-    loop_count
+    lc + (visited[guard] == facing ? 1 : 0)
   end
 
   def part_1
-    looping?(@obstacles.dup, visited = @visited.dup, @guard.dup, @facing.dup)
+    loop_count(@obstacles, visited = @visited.dup, @guard, @facing)
     visited.values.count(&:itself)
   end
 
   def part_2
-    loop_count(@obstacles.dup, visited = @visited.dup, @guard.dup, @facing.dup)
+    loop_count(@obstacles, @visited.dup, @guard, @facing, true)
   end
 end
 
