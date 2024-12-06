0a4d10b65ad9dea12d5cb3bb15b351e2196fb086
Lisa Ugray <lisa.ugray@gmail.com>
2024-12-06 09:07:40 -0500

    Day 06

---
+++ 2024/day06.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+
+require_relative 'day'
+
+class Day06 < Day
+  UP = 0
+  RIGHT = 1
+  DOWN = 2
+  LEFT = 3
+  DIRS = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+  def initialize
+    @obstacles = {}
+    @visited = {}
+    @guard = nil
+    @facing = UP
+    input_lines.each_with_index do |line, i|
+      line.each_char.each_with_index do |c, j|
+        @visited[[i, j]] = false
+        case c
+        when '#'
+          @obstacles[[i, j]] = true
+        when '^'
+          @guard = [i, j]
+          @visited[@guard] = UP
+        end
+      end
+    end
+  end
+
+  def tour(obstacles, visited, guard, facing)
+    until visited[guard].nil?
+      while !obstacles[[guard[0] + DIRS[facing][0], guard[1] + DIRS[facing][1]]]
+        guard[0] += DIRS[facing][0]
+        guard[1] += DIRS[facing][1]
+        break if visited[guard].nil?
+        return true if visited[guard] == facing
+        visited[guard] = facing
+      end
+      facing = (facing + 1) % 4
+    end
+    false
+  end
+
+  def part_1
+    tour(@obstacles.dup, visited = @visited.dup, @guard.dup, @facing.dup)
+    visited.values.count(&:itself)
+  end
+
+  def part_2
+    @visited.keys.count do |i, j|
+      next false if @obstacles[[i, j]] || @guard == [i, j]
+      obstacles = @obstacles.dup
+      obstacles[[i, j]] = true
+      tour(obstacles, @visited.dup, @guard.dup, @facing.dup)
+    end
+  end
+end
+
+Day06.run if __FILE__ == $0
