5d6d7136552224cf4b2913d4c56b47eb140741ee
Lisa Ugray <lisa.ugray@gmail.com>
2024-12-09 10:41:19 -0500

    Day 09 - refactor

--- 2024/day09.rb
+++ 2024/day09.rb
@@ -20,6 +20,7 @@ class Day09 < Day
 
   def each_id(disk_map)
     return enum_for(__method__, disk_map) unless block_given?
+    disk_map = disk_map.dup
     disk_map.pop if disk_map.size.even?
     b = back(disk_map)
     size = disk_map.each_slice(2).sum(&:first)
@@ -42,51 +43,34 @@ class Day09 < Day
     each_id(@disk_map).each_with_index.sum { |id, i| id * i }
   end
 
+  def defraged_each_id(disk_map)
+    return enum_for(__method__, disk_map) unless block_given?
+    disk_map = @disk_map.dup
+    disk_map.push(0) if disk_map.size.odd?
+    reversed = disk_map.each_slice(2).each_with_index.reverse_each.map { |(file_size, empty_size), id| [file_size, id] }
+    done = {}
+    count = 0
+    disk_map.each_slice(2).each_with_index.each do |(file_size, empty_size), id|
+      file_size.times { yield done[id] ? 0 : id }
+      done[id] = true
+      while empty_size > 0
+        break unless r = reversed.find do |file_size2, id2|
+          break nil if id2 <= id
+          file_size2 <= empty_size && !done[id2]
+        end
+        file_size2, id2 = r
+        file_size2.times { yield id2 }
+        done[id2] = true
+        empty_size -= file_size2
+      end
+      empty_size.times do
+        yield 0
+      end
+    end
+  end
+
   def part_2
-    disk = @disk_map.each_slice(2).each_with_index.flat_map do |(file_size, empty_size), i|
-      [[file_size, i], [empty_size, nil]]
-    end
-    disk.pop
-    disk.reverse_each do |size, id|
-      next unless id
-      j = disk.rindex { |_, i| i == id }
-      free_i = disk.find_index { |s, i| s >= size && i.nil? }
-      if free_i
-        next if free_i > j
-        orig_free = disk[free_i].first
-        disk[free_i] = [size, id]
-        new_free = size
-        after = disk[j+1]
-        before = disk[j-1]
-        if after && after.last.nil?
-          new_free += after.first
-        end
-        if before && before.last.nil?
-          new_free += before.first
-        end
-        disk.insert(j, [new_free, nil])
-        if after && after.last.nil?
-          disk.delete_at(j+2)
-        end
-        disk.delete_at(j+1)
-        if before && before.last.nil?
-          disk.delete_at(j-1)
-        end
-        disk.insert(free_i+1, [orig_free-size, nil])
-      end
-    end
-    disk.reject! { |size, _| size.zero? }
-    start = 0
-    disk.sum do |size, id|
-      r = if id
-        q = size * start + (size - 1) * size / 2
-        q * id
-      else
-        0
-      end
-      start += size
-      r
-    end
+    defraged_each_id(@disk_map).each_with_index.sum { |id, i| id * i }
   end
 end
 
